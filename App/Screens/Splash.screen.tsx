d905578e3c8a9ef9330e7e71a8c70b03906b92a4
Hadi Shad <hadi@mrhadi.com>
2025-01-14 15:14:29 +1100

    Update on App structure Add some libraries

---
+++ App/Screens/Splash.screen.tsx
@@ -0,0 +1,12 @@
+import React, { useContext, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+function SplashScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>News App</Text>
+    </View>
+  );
+}
+
+export default SplashScreen;
